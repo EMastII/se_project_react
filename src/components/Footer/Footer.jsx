df77771ba34a633359d3e33a8a702c411fb89b70
emast <emast8@gmail.com>
2026-07-15 16:48:15 -0500

    updated the footer to display the date instead of literal text by closing the code in curly braces

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="footer__container">
       <div className="footer__author">Developed by English Mast</div>
-      <div className="footer__year">new Date().getFullYear()</div>
+      <div className="footer__year">{new Date().getFullYear()}</div>
     </footer>
   );
 }
